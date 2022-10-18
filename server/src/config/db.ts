160df9161a8c68d63c717eccbaee98d3d930c2d0
fiyosa <fiyosa97@gmail.com>
2022-10-18 12:09:53 +0700

    fixing connect db

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config()
 const db = new Pool({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT as string),
-  database: process.env.DB_Name,
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
 })
